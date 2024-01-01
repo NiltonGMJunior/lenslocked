7304beda608cc4fdc97aaa54ea43571fed8e6485
Nilton Junior <ngm.junior@outlook.com>
2024-01-01 13:01:31 -0300

    refactor: implement pathHandler as a handler function
    Signed-off-by: Nilton Junior <ngm.junior@outlook.com>

--- main.go
+++ main.go
@@ -5,19 +5,6 @@ import (
 	"net/http"
 )
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
@@ -28,8 +15,18 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To ger in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a></p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func main() {
-	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 }
