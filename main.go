166b0d5bfa7d2dc71946dca261326dc442570150
Nilton Junior <ngm.junior@outlook.com>
2024-01-01 13:06:38 -0300

    refactor: defaulting back to router type implementation
    Signed-off-by: Nilton Junior <ngm.junior@outlook.com>

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ import (
 	"net/http"
 )
 
+type Router struct{}
+
+func (r Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		homeHandler(w, req)
+	case "/contact":
+		contactHandler(w, req)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
@@ -15,18 +28,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To ger in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
 func main() {
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", router)
 }
