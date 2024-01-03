afce150bfffe485983788cc3ce60632d4e3639cc
Nilton Gomes Martins Júnior <ngm.junior@outlook.com>
2024-01-03 16:26:44 -0300

    refactor: using embedded templates
    Signed-off-by: Nilton Gomes Martins Júnior <ngm.junior@outlook.com>

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/joncalhoun/lenslocked/controllers"
+	"github.com/joncalhoun/lenslocked/templates"
 	"github.com/joncalhoun/lenslocked/views"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
