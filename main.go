53a9f3f2b59bcb6511e8e8e8b12ea5f8dc0582d3
Nilton Junior <ngm.junior@outlook.com>
2024-10-29 12:09:24 -0300

    chore: trying out path handling
    Signed-off-by: Nilton Junior <ngm.junior@outlook.com>

--- main.go
+++ main.go
@@ -15,9 +15,15 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please send an email to <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html, charset=utf-8",)
+	fmt.Fprint(w, r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/", handlerFunc)
 	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
