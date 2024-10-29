75d2c26fd915333d73c996d62dec8876b3175ac4
Nilton Junior <ngm.junior@outlook.com>
2024-10-29 12:04:57 -0300

    feature: add contact page
    Signed-off-by: Nilton Junior <ngm.junior@outlook.com>

--- main.go
+++ main.go
@@ -6,11 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html, charset=utf-8",)
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html, charset=utf-8",)
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please send an email to <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
