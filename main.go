ca78f57e7397afa1b04b4c01a2dd500ebe185d61
Nilton Junior <ngm.junior@outlook.com>
2024-10-30 21:24:38 -0300

    refactor: refactoring the default path handler case
    Signed-off-by: Nilton Junior <ngm.junior@outlook.com>

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 		case "/contact":
 			contactHandler(w, r)
 		default:
-			http.NotFound(w, r)
+			http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
