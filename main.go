9a909bad75e6e4ae89d2acbc4e6f8400cd509012
Nilton Junior <ngm.junior@outlook.com>
2024-11-03 11:44:48 -0300

    refactor: casting path handler to a handler func
    Signed-off-by: Nilton Junior <ngm.junior@outlook.com>

--- main.go
+++ main.go
@@ -5,16 +5,6 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html, charset=utf-8",)
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html, charset=utf-8",)
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please send an email to <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
-}
-
 func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 		case "/":
@@ -26,9 +16,17 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html, charset=utf-8",)
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html, charset=utf-8",)
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please send an email to <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+}
+
 func main() {
-	http.HandleFunc("/contact", contactHandler)
-	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 }
